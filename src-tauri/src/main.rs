29bbe141e7e7f04d05f13becae8dddfc66711522
Kiwi2333 <1329634286@qq.com>
2024-05-04 22:12:08 +0800

    Fix: 更新windows打包运行会出现终端

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,52 +1,7 @@
-// use tauri::{
-//     CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
-// };
-
-
-// fn menu_handle(app_handle: &tauri::AppHandle, event: SystemTrayEvent) {
-//     match event {
-//         SystemTrayEvent::LeftClick {
-//             position: _,
-//             size: _,
-//             ..
-//         } => {
-//             println!("鼠标-左击");
-//         }
-//         SystemTrayEvent::RightClick {
-//             position: _,
-//             size: _,
-//             ..
-//         } => {
-//             println!("鼠标-右击");
-//         }
-//         SystemTrayEvent::DoubleClick {
-//             position: _,
-//             size: _,
-//             ..
-//         } => {
-//             println!("鼠标-双击");
-//         }
-//         SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-//             "quit" => {
-//                 std::process::exit(0);
-//             }
-//             "hide" => {
-//                 let item_handle = app_handle.tray_handle().get_item(&id);
-//                 let window = app_handle.get_window("home").unwrap();
-//                 if window.is_visible().unwrap() {
-//                     window.hide().unwrap();
-//                     item_handle.set_title("显示窗口").unwrap();
-//                 } else {
-//                     window.show().unwrap();
-//                     item_handle.set_title("隐藏窗口").unwrap();
-//                 }
-//             }
-//             _ => {}
-//         },
-//         _ => {}
-//     }
-// }
-
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 fn main() {
     tauri::Builder::default()
         .run(tauri::generate_context!())
