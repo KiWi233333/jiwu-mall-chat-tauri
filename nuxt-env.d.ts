0e317faf43536dc670e9ed8c585e99eacd9e17be
Kiwi2333 <1329634286@qq.com>
2024-07-05 17:27:42 +0800

    Update: 修改环境变量

--- nuxt-env.d.ts
+++ nuxt-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_ENV: "development" | "production" | "test"
   readonly VITE_PUBLIC_API_BASE_URL: string
   readonly VITE_PUBLIC_API_WS_BASE_URL: string
+  readonly VITE_PUBLIC_BASE
+  readonly VITE_PUBLIC_BASE_OSS_PATH: string
 }
 
 interface ImportMeta {
