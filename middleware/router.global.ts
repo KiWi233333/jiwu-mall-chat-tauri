cc249f7b62e687c2bd85d55e6f5cee5b1da12d1f
Kiwi2333 <1329634286@qq.com>
2024-09-01 14:10:15 +0800

    Update: 路由拦截优化

--- middleware/router.global.ts
+++ middleware/router.global.ts
@@ -2,10 +2,9 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const user = useUserStore();
   if (to.path !== "/login") {
-    if (!user.getTokenFn() || !user.isLogin)
+    if (!user.getTokenFn() || !user.isLogin) {
+      user.showLoginForm = true;
       return "/login";
-  }
-  else {
-    user.showLoginForm = true;
+    }
   }
 });
