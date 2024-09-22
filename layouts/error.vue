abf47ee1aa24e479d092cacb4edbf7bcc613eb2b
Kiwi2333 <1329634286@qq.com>
2024-09-22 14:20:12 +0800

    Update: loading和错误页面修改

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <main class="mx-a h-100dvh w-full flex-row-c-c border-default card-default">
+  <main class="mx-a h-100dvh w-full flex-row-c-c border-default card-default bg-color">
     <slot />
   </main>
 </template>
