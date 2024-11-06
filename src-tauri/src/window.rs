a3dfe6903a69db96d6d2c8c5b5033eea161a2768
Kiwi2333 <1329634286@qq.com>
2024-11-06 23:13:46 +0800

    Update: 修改窗口在不同平台的适配

--- src-tauri/src/window.rs
+++ src-tauri/src/window.rs
@@ -1,9 +1,8 @@
-// window.rs
 use tauri::{AppHandle, Manager, WebviewUrl, WebviewWindowBuilder, WindowEvent};
 
-pub fn setup_window(app: &tauri::AppHandle) -> tauri::Result<()> {
-    // 注册命令
-    let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+pub fn setup_window(app: &AppHandle) -> tauri::Result<()> {
+    // 主窗口配置
+    let mut main_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
         .title("极物聊天")
         .resizable(true)
         .center()
@@ -12,78 +11,82 @@ pub fn setup_window(app: &tauri::AppHandle) -> tauri::Result<()> {
         .min_inner_size(375.0, 780.0)
         .max_inner_size(1920.0, 1080.0)
         .inner_size(1280.0, 860.0);
-    #[cfg(any(target_os = "windows", target_os = "linux"))]
-    // url 打包前是/msg 打包后是 /msg.html
-    let msgbox_builder = WebviewWindowBuilder::new(app, "msgbox", WebviewUrl::App("/msg".into()))
+
+    // 消息窗口配置
+    let mut msgbox_builder = WebviewWindowBuilder::new(app, "msgbox", WebviewUrl::App("/msg".into()))
         .title("消息通知")
         .inner_size(240.0, 300.0)
         .skip_taskbar(true)
         .decorations(false)
-        .transparent(true)
         .resizable(false)
         .always_on_top(true)
         .shadow(false)
         .position(0.0, 0.0)
         .visible(false);
-    // 仅在 Windows 和 Linux 时设置透明背景
-    #[cfg(any(target_os = "windows", target_os = "linux"))]
-    let msgbox_builder = msgbox_builder.transparent(true);
-    #[cfg(any(target_os = "windows", target_os = "linux"))]
-    let win_builder = win_builder.transparent(true);
-    // 仅在 macOS 时设置透明标题栏
-    #[cfg(target_os = "macos")]
-    let win_builder = win_builder.title_bar_style(tauri::utils::TitleBarStyle::Transparent);
 
-    let _main_window = win_builder.build().unwrap();
-    let _msgbox = msgbox_builder.build().unwrap();
+    // Windows 和 Linux 平台特定配置
+    #[cfg(any(target_os = "windows", target_os = "linux"))]
+    {
+        main_builder = main_builder.transparent(true);
+        msgbox_builder = msgbox_builder.transparent(true);
+    }
+
+    // macOS 平台特定配置
+    #[cfg(target_os = "macos")]
+    {
+        use tauri::utils::TitleBarStyle;
+        main_builder = main_builder.title_bar_style(TitleBarStyle::Transparent);
+        msgbox_builder = msgbox_builder.title_bar_style(TitleBarStyle::Hidden);
+    }
+
+    // 构建主窗口和消息窗口
+    let main_window = main_builder.build()?;
+    #[cfg(any(target_os = "windows", target_os = "linux",  target_os = "macos"))]
+    let msgbox_window = msgbox_builder.build()?;
+
     // 监听窗口事件
-    _msgbox.clone().on_window_event(move |event| match event {
-        // WindowEvent::Resized(size) => {
-        //     println!("窗口调整大小为：宽 {} 高 {}", size.width, size.height);
-        // }
-        // WindowEvent::Moved(position) => {
-        //     println!("窗口移动到位置：x {} y {}", position.x, position.y);
-        // }
+    #[cfg(any(target_os = "windows", target_os = "linux",  target_os = "macos"))]
+    msgbox_window.clone().on_window_event(move |event| match event {
         WindowEvent::CloseRequested { api, .. } => {
             println!("关闭请求，窗口将最小化而不是关闭。");
             api.prevent_close();
         }
         WindowEvent::Focused(focused) => {
-            if *focused {
-            } else {
-                _msgbox.hide().unwrap();
+            if !*focused {
+                msgbox_window.hide().unwrap_or_else(|e| eprintln!("隐藏窗口时出错: {:?}", e));
             }
         }
         _ => {}
     });
 
-    // 仅在构建 macOS 时设置背景颜色
+    // macOS 平台设置窗口背景颜色
     #[cfg(target_os = "macos")]
     {
-        use cocoa::appkit::{NSColor};
+        use cocoa::appkit::NSColor;
         use cocoa::base::{id, nil};
 
-        let ns_window = _main_window.ns_window().unwrap() as id;
-        unsafe {
-            let bg_color = NSColor::colorWithRed_green_blue_alpha_(
-                nil,
-                50.0 / 255.0,
-                158.0 / 255.0,
-                163.5 / 255.0,
-                1.0,
-            );
-            ns_window.setBackgroundColor_(bg_color);
+        if let Some(ns_window) = main_window.ns_window() {
+            unsafe {
+                let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+                    nil,
+                    50.0 / 255.0,
+                    158.0 / 255.0,
+                    163.5 / 255.0,
+                    1.0,
+                );
+                let ns_window = ns_window as id;
+                ns_window.setBackgroundColor_(bg_color);
+            }
         }
     }
+
     Ok(())
 }
 
 pub fn show_window(app: &AppHandle) {
-    let windows = app.webview_windows(); // This should now work
-    windows
-        .values()
-        .next()
-        .expect("Sorry, no window found")
-        .set_focus()
-        .expect("Can't Bring Window to Focus");
+    if let Some(window) = app.webview_windows().values().next() {
+        window.set_focus().unwrap_or_else(|e| eprintln!("设置窗口焦点时出错: {:?}", e));
+    } else {
+        eprintln!("未找到窗口");
+    }
 }
