5b384a843aa558b1476751eb2cb8f741a99b0143
Kiwi2333 <1329634286@qq.com>
2025-01-03 15:03:12 +0800

    Update: 安卓预获取`录音`和`相机`权限

--- src-tauri/gen/android/app/src/main/java/com/jiwuchat/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/jiwuchat/app/MainActivity.kt
@@ -1,3 +1,34 @@
 package com.jiwuchat.app
 
-class MainActivity : TauriActivity()
+import android.Manifest
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : TauriActivity() {
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach { entry ->
+                val permissionName = entry.key
+                val isGranted = entry.value
+                // 处理权限请求结果
+                // if (isGranted) {
+                // } else {
+                // }
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 请求权限
+        requestPermissions()
+    }
+
+    private fun requestPermissions() {
+        val permissions = arrayOf(
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA,
+        )
+        requestPermissionLauncher.launch(permissions)
+    }
+}
