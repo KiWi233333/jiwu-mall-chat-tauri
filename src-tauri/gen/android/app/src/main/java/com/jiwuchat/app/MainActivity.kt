b09e3af96171a83c4f355e7d1b9cc9fe80c2e270
Kiwi2333 <1329634286@qq.com>
2025-02-19 21:17:51 +0800

    Fix(mobile): 修复安卓移动端输入框与键盘遮挡问题

--- src-tauri/gen/android/app/src/main/java/com/jiwuchat/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/jiwuchat/app/MainActivity.kt
@@ -2,36 +2,41 @@ package com.jiwuchat.app
 
 import android.Manifest
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : TauriActivity() {
 
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-            permissions.entries.forEach { entry ->
-                val permissionName = entry.key
-                val isGranted = entry.value
-                // 处理权限请求结果
-                // if (isGranted) {
-                // } else {
-                // }
-            }
-        }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // 请求权限
-        requestPermissions()
+  private val requestPermissionLauncher =
+    registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+      permissions.entries.forEach { entry ->
+        val permissionName = entry.key
+        val isGranted = entry.value
+      }
     }
 
-    private fun requestPermissions() {
-        requestPermissionLauncher.launch(arrayOf(
-          Manifest.permission.RECORD_AUDIO,
-          Manifest.permission.CAMERA,
-          Manifest.permission.WRITE_EXTERNAL_STORAGE,
-          Manifest.permission.READ_EXTERNAL_STORAGE,
-          Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
-          Manifest.permission.FOREGROUND_SERVICE
-        ))
-    }
+  override fun onWebViewCreate(webView: WebView) {
+    super.onWebViewCreate(webView)
+    // 字体不可缩放
+    webView.settings.textZoom = 100;
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // 请求权限
+    requestPermissions()
+  }
+
+  private fun requestPermissions() {
+    requestPermissionLauncher.launch(
+      arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+        Manifest.permission.FOREGROUND_SERVICE
+      )
+    )
+  }
 }
